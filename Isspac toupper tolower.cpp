15b1ecffeda0c169f99bbaaa95dac55f95c5b0ce
kawsar42 <59926691+kawsar42@users.noreply.github.com>
2022-09-04 20:47:05 +0600

    Add files via upload

---
+++ Isspac toupper tolower.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    char c1=' ';
+    bool f1=isspace(c1);
+    cout << f1 << endl;
+    char c2='A';
+    bool f2=isupper(c2);
+    cout << f2<< endl;
+    char c3='a';
+    bool f3=islower(c3);
+    cout << f3 << endl;
+    char c4='a';
+    c4=toupper(c4);
+    cout <<c4 << endl;
+    char c5='A';
+    c5=tolower(c5);
+    cout << c5 << endl;
+}
+
